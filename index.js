c064fa221b89018225debbc9184787656d07fbed
ntan1 <nctan1002@gmail.com>
2018-01-28 10:56:38 -0500

    added claim functionality

--- index.js
+++ index.js
@@ -79,18 +79,17 @@ MongoClient.connect('mongodb://admin:admin@ds117888.mlab.com:17888/deltahacksdat
     // claim button function
     app.post('/claim_food', function(req, res, next) {
         var id = req.body._id;
-        console.log(id);
-            db.collection('logins').updateOne(
-                {"_id": id},{$set: { "claimed": true }},
-                function (err, r) {
-                    assert.equal(null, err);
-                    db.collection('logins').find({claimed: {$exists: true}}).toArray(
-                        function(err, foodPosts) {
-                            res.render('shelters.html', {'foodPosts': foodPosts, 'Shelter': 'Angels'});
-                        }
-                    )
-                }
-            );
+        db.collection('logins').updateOne(
+            {_id: new ObjectID(id)},{$set: { claimed: true }},
+            function (err, r) {
+                assert.equal(null, err);
+                db.collection('logins').find({claimed: {$exists: true}}).toArray(
+                    function(err, foodPosts) {
+                        res.render('shelters.html', {'foodPosts': foodPosts, 'Shelter': 'Angels'});
+                    }
+                )
+            }
+        );
     });
 
     app.use(errorHandler);
