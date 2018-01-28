a400690e31a61b23eed6d45208e497529feb8ae0
Sangeet Parashar <sparasha@uwo.ca>
2018-01-27 20:45:02 -0500

    index.html fixes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require('path');
 
 // viewed at http://localhost:8080
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
